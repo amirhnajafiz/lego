7230fbb86b9d790dd75a232f6cc60f93a80fc33a
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-17 18:12:36 +0330

    basic server is set

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,3 +4,32 @@ It sets the server up with its configurations.
 */
 
 package server
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Configuration of routes
+	http.HandleFunc("/", handler)
+
+	// Starting the server
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Printf("Server is listening on 127.0.0.1:8080 ...")
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	message := "Server response"
+	if r.Method != "get" {
+		message = "Bad request"
+	}
+	_, _ = io.WriteString(w, message)
+}
