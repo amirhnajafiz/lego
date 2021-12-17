76787519de5272518e120c0b208649e46014bccb
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-17 18:22:08 +0330

    server started successfully

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,7 +3,7 @@ This is the starter of the project.
 It sets the server up with its configurations.
 */
 
-package server
+package main
 
 import (
 	"fmt"
@@ -17,18 +17,17 @@ func main() {
 	http.HandleFunc("/", handler)
 
 	// Starting the server
+	fmt.Println("Server is listening on 127.0.0.1:8080 ...")
 	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
 		log.Fatal(err)
-	} else {
-		fmt.Printf("Server is listening on 127.0.0.1:8080 ...")
 	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	message := "Server response"
-	if r.Method != "get" {
+	if r.Method != "GET" {
 		message = "Bad request"
 	}
 	_, _ = io.WriteString(w, message)
