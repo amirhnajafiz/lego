a88f715b68b9dfdb8255cec14b39d6acedbed43d
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-17 18:52:06 +0330

    bind route is now returning json

--- routes/bind/main.go
+++ routes/bind/main.go
@@ -7,12 +7,23 @@ and binds it with "Hello {name}" message.
 package bind
 
 import (
-	"io"
+	"encoding/json"
 	"net/http"
+	"time"
+
+	B "cmd/internal/bind-response"
 )
 
 // Bind handles the "/bind" route
 func Bind(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("name")
-	_, _ = io.WriteString(w, "Hello "+name)
+
+	data := B.BindResponse{}
+	data.Message = "Hello " + name
+	data.Time = time.Now()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+
+	_ = json.NewEncoder(w).Encode(data)
 }
