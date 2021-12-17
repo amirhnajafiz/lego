9cc577776af1d722f9dc7f3e452c27d8785d1395
amirhnajafiz <najafizadeh21@gmail.com>
2021-12-17 19:36:49 +0330

    k6 added to project

---
+++ api/k6/script.js
@@ -0,0 +1,45 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { Counter } from 'k6/metrics';
+
+// A simple counter for http requests
+
+export const requests = new Counter('http_reqs');
+
+// you can specify stages of your test (ramp up/down patterns) through the options object
+// target is the number of VUs you are aiming for
+
+export const options = {
+    stages: [
+        { target: 20, duration: '1m' },
+        { target: 15, duration: '1m' },
+        { target: 0, duration: '1m' },
+    ],
+    thresholds: {
+        requests: ['count < 100'],
+    },
+};
+
+export default function () {
+    // our HTTP request, note that we are saving the response to res, which can be accessed later
+
+    let res = http.get('http://127.0.0.1:8080');
+
+    sleep(1);
+
+    let checkRes = check(res, {
+        'status is 200': (r) => r.status === 202,
+    });
+
+    console.log(checkRes)
+
+    res = http.get('http://127.0.0.1:8080/bind?name=Amir');
+
+    sleep(1);
+
+    checkRes = check(res, {
+        'status is 200': (r) => r.status === 202,
+    });
+
+    console.log(checkRes)
+}
