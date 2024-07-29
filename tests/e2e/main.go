1b2d7772a1aeb1cc862d1cd5755d2f958f81768e
amirhnajafiz <najafizadeh21@gmail.com>
2024-07-29 10:41:08 +0330

    add: e2e test

--- tests/e2e/main.go
+++ tests/e2e/main.go
@@ -1,41 +1,115 @@
 package main
 
 import (
-	"bufio"
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"net/url"
 )
 
+// create base variables
 var (
-	baseURL     = "http://localhost:8080"
-	healthPath  = "/healthz"
-	metricsPath = "/metrics"
+	baseURL = "http://localhost:8080"
+
+	ktest = "test-k"
+	vtest = "test-v"
+
+	errServer   = errors.New("server failed to respond")
+	errResponse = errors.New("unexpected response")
 )
+
+// defining our testing types
+type testingFunc func() error
+
+// create testing methods
+func TestPost() error {
+	uri := url.URL{
+		Host: fmt.Sprintf("%s/v1/new", baseURL),
+	}
+
+	uri.Query().Add("key", ktest)
+	uri.Query().Add("value", vtest)
+
+	req, _ := http.NewRequest(http.MethodPost, uri.String(), nil)
+	client := http.Client{}
+
+	if resp, err := client.Do(req); err != nil {
+		log.Println(err)
+
+		return errResponse
+	} else {
+		if resp.StatusCode != 200 {
+			return errServer
+		}
+	}
+
+	return nil
+}
+
+func TestGet() error {
+	uri := url.URL{
+		Host: fmt.Sprintf("%s/v1/get", baseURL),
+	}
+
+	uri.Query().Add("key", ktest)
+
+	req, _ := http.NewRequest(http.MethodGet, uri.String(), nil)
+	client := http.Client{}
+
+	if resp, err := client.Do(req); err != nil {
+		log.Println(err)
+
+		return errResponse
+	} else {
+		if resp.StatusCode != 200 {
+			return errServer
+		}
+
+		data := make([]byte, 0)
+		if _, err := resp.Body.Read(data); err == nil {
+			if string(data) != vtest {
+				return errResponse
+			}
+		}
+	}
+
+	return nil
+}
+
+func TestDelete() error {
+	uri := url.URL{
+		Host: fmt.Sprintf("%s/v1/del", baseURL),
+	}
+
+	uri.Query().Add("key", ktest)
+
+	req, _ := http.NewRequest(http.MethodDelete, uri.String(), nil)
+	client := http.Client{}
+
+	if resp, err := client.Do(req); err != nil {
+		log.Println(err)
+
+		return errResponse
+	} else {
+		if resp.StatusCode != 200 {
+			return errServer
+		}
+	}
+
+	return nil
+}
 
 func main() {
-	resp, err := http.Get(baseURL + healthPath)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("Response status:", resp.Status)
-
-	scanner := bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan(); i++ {
-		fmt.Println(scanner.Text())
+	methods := []testingFunc{
+		TestPost,
+		TestGet,
+		TestDelete,
 	}
 
-	resp, err = http.Get(baseURL + metricsPath)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	fmt.Println("Response status:", resp.Status)
-
-	scanner = bufio.NewScanner(resp.Body)
-	for i := 0; scanner.Scan(); i++ {
-		fmt.Println(scanner.Text())
+	for _, method := range methods {
+		if err := method(); err != nil {
+			panic(err)
+		}
 	}
 }
