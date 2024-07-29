00711bc256c4e52377fda5fb2776c74e42fb1a0b
amirhnajafiz <najafizadeh21@gmail.com>
2024-07-29 10:19:25 +0330

    feat: logging package

--- internal/monitoring/logging/console.go
+++ internal/monitoring/logging/console.go
@@ -1,17 +1,33 @@
 package logging
 
+import "log"
+
 // consoleLogr supports Logr interface. It displays the output to the process
 // stdout (console).
 type consoleLogr struct{}
 
 func (c consoleLogr) Info(messages ...string) {
+	format := "[[info]] %s\n"
 
+	for _, msg := range messages {
+		log.Printf(format, msg)
+	}
 }
 
 func (c consoleLogr) Warning(messages ...string) {
+	format := "[[warning]] %s\n"
 
+	for _, msg := range messages {
+		log.Printf(format, msg)
+	}
 }
 
 func (c consoleLogr) Error(err error, messages ...string) {
+	format := "[[error]] %s\n"
 
+	log.Println(err)
+
+	for _, msg := range messages {
+		log.Printf(format, msg)
+	}
 }
