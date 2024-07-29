4688b9fb8ce30b43bc9e036bd601e588d78ab23a
amirhnajafiz <najafizadeh21@gmail.com>
2024-07-29 10:23:06 +0330

    update: middleware logging

--- internal/http/middlewares/log_per_request.go
+++ internal/http/middlewares/log_per_request.go
@@ -1,13 +1,13 @@
 package middlewares
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 )
 
 func (m MiddlewaresManager) LogPerRequest(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("[%s] %s\n", r.Method, r.URL.Path)
+		m.Logr.Info(fmt.Sprintf("[%s] %s\n", r.Method, r.URL.Path))
 
 		next.ServeHTTP(w, r)
 	})
--- internal/http/middlewares/manager.go
+++ internal/http/middlewares/manager.go
@@ -1,9 +1,13 @@
 package middlewares
 
-import "github.com/amirhnajafiz/letsgo/internal/monitoring/metrics"
+import (
+	"github.com/amirhnajafiz/letsgo/internal/monitoring/logging"
+	"github.com/amirhnajafiz/letsgo/internal/monitoring/metrics"
+)
 
 // MiddlewaresManager contains middleware methods that are being used
 // after the http router and before handlers.
 type MiddlewaresManager struct {
 	Metrics metrics.Metrics
+	Logr    logging.Logr
 }
