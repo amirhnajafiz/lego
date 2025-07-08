8b9b2bbbc1d874202f4748cf4de2131128a3c596
amirhnajafiz <najafizadeh21@gmail.com>
2025-07-07 21:44:18 -0400

    update: lego files

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/amirhnajafiz/letsgo
-
-go 1.20
--- internal/http/handlers/v1/responses.go
+++
@@ -1,8 +0,0 @@
-package v1
-
-// objectResponse is a struct used for returning user
-// get request as a JSON object.
-type objectResponse struct {
-	Key    string `json:"key"`
-	Values string `json:"value"`
-}
