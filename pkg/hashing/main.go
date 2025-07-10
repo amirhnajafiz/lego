788675b42ad11577e9d47ce6578a8d72c043fa34
amirhnajafiz <najafizadeh21@gmail.com>
2025-07-09 20:32:27 -0400

    add: hashing methods

--- pkg/hashing/main.go
+++ pkg/hashing/main.go
@@ -2,6 +2,7 @@ package hashing
 
 import (
 	"crypto/md5"
+	"crypto/sha256"
 	"encoding/hex"
 )
 
@@ -11,6 +12,23 @@ import (
 func MD5(input string) string {
 	// create a new MD5 hash
 	hash := md5.New()
+
+	// write the input string to the hash
+	hash.Write([]byte(input))
+
+	// get the resulting hash as a byte slice
+	hashBytes := hash.Sum(nil)
+
+	// convert the byte slice to a hexadecimal string
+	return hex.EncodeToString(hashBytes)
+}
+
+// SHA256 computes the SHA256 hash of the input string and returns it as a hexadecimal string.
+// It uses the crypto/sha256 package to create a new SHA256 hash, writes the input string to the hash,
+// and then converts the resulting byte slice to a hexadecimal string.
+func SHA256(input string) string {
+	// create a new SHA256 hash
+	hash := sha256.New()
 
 	// write the input string to the hash
 	hash.Write([]byte(input))
